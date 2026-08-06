7ceba4149d044e0e8d641e2a6dc46d7d84299330
vergilantte <xvergilantte@gmail.com>
2026-08-06 07:03:08 +0700

    ak3: initialize Yamato
    Signed-off-by: vergilantte <xvergilantte@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,22 +4,16 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Yamato kernel by vergilantte
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
-do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
-supported.versions=
-supported.patchlevels=
-supported.vendorpatchlevels=
+do.cleanuponabort=1
+device.name1=surya
+device.name2=karna
+supported.versions=11-17
 '; } # end properties
-
 
 ### AnyKernel install
 ## boot files attributes
@@ -28,8 +22,58 @@ set_perm_recursive 0 0 755 644 $RAMDISK/*;
 set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
+# begin build.prop loader
+load_build_props() {
+if [ ! -f "/system/build.prop" ] && [ ! -f "/system_root/system/build.prop" ]; then
+	mount /system 2>/dev/null || mount /system_root 2>/dev/null
+fi
+
+if [ -f "/system/build.prop" ]; then
+	SYSTEM_BUILD_PROP="/system/build.prop"
+elif [ -f "/system_root/system/build.prop" ]; then
+	SYSTEM_BUILD_PROP="/system_root/system/build.prop"
+fi
+
+if [ -n "$SYSTEM_BUILD_PROP" ]; then
+	PROP_MIUI=$(file_getprop "$SYSTEM_BUILD_PROP" ro.miui.ui.version.code)
+fi
+
+if [ -f "$AKHOME/android_ver" ]; then
+	ANDROID_VERSION=$(cat "$AKHOME/android_ver")
+fi
+} # end build.prop loader
+
+# begin legacy bootargs patch
+patch_legacy_bootargs() {
+
+ui_print " "
+
+if [ -n "$PROP_MIUI" ]; then
+	ui_print "MIUI $PROP_MIUI detected, defaulting to legacy bootargs"
+	patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=1
+	patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=1
+	return
+fi
+
+if [ "$ANDROID_VERSION" -lt 15 ]; then
+	ui_print "Enabling legacy eBPF bootarg..."
+	patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=1
+else
+	ui_print "Disabling legacy eBPF bootarg..."
+	patch_cmdline init.is_legacy_ebpf init.is_legacy_ebpf=0
+fi
+
+if [ "$ANDROID_VERSION" -lt 13 ]; then
+	ui_print "Enabling legacy timestamp bootarg..."
+	patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=1
+else
+	ui_print "Disabling legacy timestamp bootarg..."
+	patch_cmdline init.is_legacy_timestamp init.is_legacy_timestamp=0
+fi
+} # end legacy bootargs patch
+
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+BLOCK=/dev/block/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
@@ -37,85 +81,18 @@ PATCH_VBMETA_FLAG=auto;
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
 
+# load build.prop properties
+load_build_props;
+
+# replace dtbo if MIUI is detected
+if [ -n "$PROP_MIUI" ] && [ -f "$AKHOME/dtbo-miui.img" ]; then
+	ui_print " "
+	ui_print "Replacing dtbo with MIUI-specific image..."
+	mv "$AKHOME/dtbo-miui.img" "$AKHOME/dtbo.img"
+fi
+
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
-
+patch_legacy_bootargs; # patch legacy bootargs based on Android version
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
-
-
-## init_boot files attributes
-#init_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# init_boot shell variables
-#BLOCK=init_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for init_boot patching
-#reset_ak;
-
-# init_boot install
-#dump_boot; # unpack ramdisk since it is the new first stage init ramdisk where overlay.d must go
-
-#write_boot;
-## end init_boot install
-
-
-## vendor_kernel_boot shell variables
-#BLOCK=vendor_kernel_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_kernel_boot patching
-#reset_ak;
-
-# vendor_kernel_boot install
-#split_boot; # skip unpack/repack ramdisk, e.g. for dtb on devices with hdr v4 and vendor_kernel_boot
-
-#flash_boot;
-## end vendor_kernel_boot install
-
-
-## vendor_boot files attributes
-#vendor_boot_attributes() {
-#set_perm_recursive 0 0 755 644 $RAMDISK/*;
-#set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
-#} # end attributes
-
-# vendor_boot shell variables
-#BLOCK=vendor_boot;
-#IS_SLOT_DEVICE=1;
-#RAMDISK_COMPRESSION=auto;
-#PATCH_VBMETA_FLAG=auto;
-
-# reset for vendor_boot patching
-#reset_ak;
-
-# vendor_boot install
-#dump_boot; # use split_boot to skip ramdisk unpack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-
-#write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
-## end vendor_boot install
-
